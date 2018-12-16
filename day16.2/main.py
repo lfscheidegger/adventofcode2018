5f6f65f059bf506ed41359239033b347b2de92e5
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2018-12-16 15:16:38 -0800

    Adding descriptions to day 16

--- day16.2/main.py
+++ day16.2/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 """
+--- Part Two ---
+Using the samples you collected, work out the number of each opcode and execute the test program (the second section of your puzzle input).
+
+What value is contained in register 0 after executing the test program?
 """
 
 from collections import defaultdict
